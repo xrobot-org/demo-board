10c6411cce11bedb736d004e1beb23af7c35afc2
xiao <2592509183@qq.com>
2023-03-09 14:22:56 +0800

    first commit

---
+++ board.cmake
@@ -0,0 +1,23 @@
+cmake_minimum_required(VERSION 3.11)
+
+set(BOARD_NAME MiniPC)
+
+add_subdirectory(${BOARD_DIR}/drivers)
+
+add_executable(${PROJECT_NAME}.elf ${BOARD_DIR}/main.cpp)
+
+target_link_libraries(
+  ${PROJECT_NAME}.elf
+  PUBLIC bsp
+  PUBLIC system
+  PUBLIC robot
+  )
+
+
+target_include_directories(
+  ${PROJECT_NAME}.elf
+  PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}
+  PRIVATE $<TARGET_PROPERTY:bsp,INTERFACE_INCLUDE_DIRECTORIES>
+  PRIVATE $<TARGET_PROPERTY:system,INTERFACE_INCLUDE_DIRECTORIES>
+  PRIVATE $<TARGET_PROPERTY:robot,INTERFACE_INCLUDE_DIRECTORIES>
+  )
